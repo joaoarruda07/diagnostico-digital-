fd8a7a8a547a3abfb65d1a27b9eaa003e6961548
joaoarruda07 <comercial@solutionmedica.com>
2026-06-04 17:11:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div style={{ width: '100%', maxWidth: '960px', margin: '0 auto' }}>
-      <App />
-    </div>
+    <App />
   </StrictMode>
 )
